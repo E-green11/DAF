5fb1e7fa55fcb2a708756b8953407bc52394b868
E-green11 <147219083+E-green11@users.noreply.github.com>
2025-09-24 15:22:44 +0800

    Update train.sh

--- DAF/train.sh
+++ DAF/train.sh
@@ -23,7 +23,7 @@ low_rank_dim=8
 SEED=0
 dynamic_update_interval=10
 param_budget=0.2
-sensitivity_exp_name="dynamic_spt_lora_a${alpha}_b${beta}"
+sensitivity_exp_name="dynamic_DAF_lora_a${alpha}_b${beta}"
 
 for LR in 0.0003
 do
@@ -31,9 +31,9 @@ do
     do
         for WEIGHT_DECAY in 0.0001
         do
-            exp_name=vtab_dynamic_spt_lora_a${alpha}_b${beta}_i${dynamic_update_interval}_p${param_budget}
+            exp_name=vtab_dynamic_DAF_lora_a${alpha}_b${beta}_i${dynamic_update_interval}_p${param_budget}
             export MASTER_PORT=$((12000 + $RANDOM % 20000))
-            python train_dynamic_spt.py --data-path=./data/vtab-1k/${DATASET} --data-set=${DATASET} --model_name=vit_base_patch16_224_in21k_spt --resume=checkpoints/ViT-B_16.npz --output_dir=./saves/${DATASET}_dynamic_spt_lr-${LR}_wd-${WEIGHT_DECAY} --batch-size=64 --lr=${LR} --epochs=200 --weight-decay=${WEIGHT_DECAY} --no_aug --mixup=0 --cutmix=0 --direct_resize --smoothing=0 --launcher="none" --seed=${SEED} --val_interval=5  --opt=adamw --low_rank_dim=${low_rank_dim} --initial_sensitivity_path=sensitivity_${sensitivity_exp_name}/${DATASET}/param_req_${param_budget}.pth --exp_name=${currenttime}-${exp_name} --seed=0 --test --block=BlockSPTParallel  --structured_type=lora --structured_vector --freeze_stage --dynamic_update_interval=${dynamic_update_interval} --param_budget=${param_budget} --alpha=${alpha} --beta=${beta} | tee -a logs/${currenttime}-${exp_name}.log
+            python train_dynamic_DAF.py --data-path=./data/vtab-1k/${DATASET} --data-set=${DATASET} --model_name=vit_base_patch16_224_in21k_DAF --resume=checkpoints/ViT-B_16.npz --output_dir=./saves/${DATASET}_dynamic_DAF_lr-${LR}_wd-${WEIGHT_DECAY} --batch-size=64 --lr=${LR} --epochs=200 --weight-decay=${WEIGHT_DECAY} --no_aug --mixup=0 --cutmix=0 --direct_resize --smoothing=0 --launcher="none" --seed=${SEED} --val_interval=5  --opt=adamw --low_rank_dim=${low_rank_dim} --initial_sensitivity_path=sensitivity_${sensitivity_exp_name}/${DATASET}/param_req_${param_budget}.pth --exp_name=${currenttime}-${exp_name} --seed=0 --test --block=BlockDAFParallel  --structured_type=lora --structured_vector --freeze_stage --dynamic_update_interval=${dynamic_update_interval} --param_budget=${param_budget} --alpha=${alpha} --beta=${beta} | tee -a logs/${currenttime}-${exp_name}.log
         done
     done
 done 
